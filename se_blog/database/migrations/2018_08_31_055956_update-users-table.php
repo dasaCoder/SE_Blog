962dead1c8151d308f4598e41b5ccab301dda224
dasaCoder <dilushadasanayaka@gmail.com>
2018-08-31 12:05:56 +0530

    fixing curl error

--- se_blog/database/migrations/2018_08_31_055956_update-users-table.php
+++ se_blog/database/migrations/2018_08_31_055956_update-users-table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class UpdateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->string('email')->nullable()->change();
+            $table->string('password', 60)->nullable()->change();
+            $table->string('provider');
+            $table->string('provider_id');
+
+        });
     }
 
     /**

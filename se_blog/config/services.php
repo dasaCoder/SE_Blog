b87995fc5769e1be0104c0447f74fb89bbb51701
Inthujan14 <inthujan332@gmail.com>
2018-09-01 02:00:04 +0530

    edit config

--- se_blog/config/services.php
+++ se_blog/config/services.php
@@ -37,13 +37,15 @@ return [
     'github' => [
         'client_id' => env('78d7db8a81c0fe755820'),         // Your GitHub Client ID
         'client_secret' => env('8d85a42c7c3be010f2d1647c1e68ace8bef935ee'), // Your GitHub Client Secret
-        'redirect' => 'http://localhost/se_blog/se_blog/public/login/github/callback',
+        'redirect' => 'http://localhost:8000/login/github/callback',
+        /*'redirect' => 'http://localhost/se_blog/se_blog/public/login/github/callback',*/
     ],
 
     'facebook' => [
         'client_id' => '317817035433027',
         'client_secret' => 'c9c4be37c579bb9447cc67d25c5f3ea1',
-        'redirect' => 'http://localhost/se_blog/se_blog/public/login/facebook/callback',
+        'redirect' => 'http://localhost:8000/login/facebook/callback',
+        /*'redirect' => 'http://localhost/se_blog/se_blog/public/login/facebook/callback',*/
     ],
 
 
